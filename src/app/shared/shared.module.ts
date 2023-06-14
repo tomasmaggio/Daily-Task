996bdf7da97cffc4db4bb3fae68b5d32ac2a5384
tomasmaggio <tomasmaggio220@gmail.com>
2023-06-14 11:32:43 -0300

    creamos componentes compartidos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
