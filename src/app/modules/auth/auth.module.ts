6866ad1bcf5596b795331f698bd475e32f738efe
tomasmaggio <tomasmaggio220@gmail.com>
2023-06-14 11:48:38 -0300

    Creacion del login y register

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagesComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule
   ]
